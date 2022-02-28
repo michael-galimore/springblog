f40398599bcb978c789de1f635dc2e2104906803
michael galimore <michael.galimore1@gmail.com>
2022-02-27 18:27:44 -0600

    finished views exercise

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -20,8 +21,9 @@ public class HelloController {
 // REQUEST MAPPING
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
-    public String increment(@PathVariable int number){
+    public String increment(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
+
 
     }
 
--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,28 +1,43 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
     @ResponseBody
-    public String posts(){
-        return "posts index page";
+    public String viewPosts(Model model){
+        ArrayList<Post> allPosts = new ArrayList<>();
+        Post post2 = new Post(2, "test", "can you see me?");
+        Post post3 = new Post(2, "test", "can you see me?");
+        Post post4 = new Post(2, "test", "can you see me?");
+
+        allPosts.add(post2);
+        allPosts.add(post3);
+        allPosts.add(post4);
+        model.addAttribute("allpost",allPosts);
+        return "posts/index";
     }
 
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable long id){
-        return "view an individual post";
+//    @ResponseBody
+    public String individualPost(@PathVariable long id, Model model){
+        Post post1 = new Post(1, "title", "Hi there");
+        model.addAttribute("singlePost", post1);
+
+        return "/posts/show";
     }
 
 
     @GetMapping("/posts/create")
     @ResponseBody
     public String showCreateForm(){
-        return "view the form for creating a post";
+        return "post/create";
     }
 
     @PostMapping("/posts/create")
